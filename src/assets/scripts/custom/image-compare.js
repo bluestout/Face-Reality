4e2c6ae84db0faadec96b6f659cf4fbc341b095a
Matjaz Zorko <matjaz@apto.si>
2018-11-12 13:34:11 +0100

    initialise and run each image comparer on its own

--- src/assets/scripts/custom/image-compare.js
+++ src/assets/scripts/custom/image-compare.js
@@ -1,23 +1,27 @@
 import $ from 'jquery';
 
-function compareSetPosition(value) {
-  $('[data-compare-after-wrap]').css('left', `${value}%`);
-  $('[data-compare-after-image]').css('left', `-${value}%`);
-  $('[data-compare-visual-slide]').css('left', `${value}%`);
+function compareSetPosition($base, value) {
+  $base.find('[data-compare-after-wrap]').css('left', `${value}%`);
+  $base.find('[data-compare-after-image]').css('left', `-${value}%`);
+  $base.find('[data-compare-visual-slide]').css('left', `${value}%`);
 }
 
 function compareChanged(event) {
   const pos = event.currentTarget.valueAsNumber;
+  const $base = $(event.currentTarget).closest('[data-compare-base]');
   if (typeof pos === 'number' && pos > -1 && pos < 101) {
-    compareSetPosition(pos);
+    compareSetPosition($base, pos);
   }
 }
 
 $(document).on('input', '[data-compare-range]', compareChanged);
 
 $(document).ready(() => {
-  const base = $('[data-compare-base]').data('compare-base');
-  if (typeof base === 'number' && base > -1 && base < 101) {
-    compareSetPosition(base);
-  }
+  $('[data-compare-base]').each(function() {
+    const $base = $(this);
+    const pos = $base.data('compare-base');
+    if (typeof pos === 'number' && pos > -1 && pos < 101) {
+      compareSetPosition($base, pos);
+    }
+  });
 });
