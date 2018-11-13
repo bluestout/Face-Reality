d7c8441ff11478f7a17e11ea772a0ba51635afaa
Matjaz Zorko <matjaz@apto.si>
2018-11-13 17:07:04 +0100

    Cart & related sections slicing & implement

--- src/assets/scripts/layout/theme.js
+++ src/assets/scripts/layout/theme.js
@@ -1,29 +1,30 @@
-import 'lazysizes/plugins/object-fit/ls.object-fit';
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import 'lazysizes/plugins/rias/ls.rias';
-import 'lazysizes/plugins/bgset/ls.bgset';
-import 'lazysizes';
-import 'lazysizes/plugins/respimg/ls.respimg';
+import "lazysizes/plugins/object-fit/ls.object-fit";
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
+import "lazysizes/plugins/rias/ls.rias";
+import "lazysizes/plugins/bgset/ls.bgset";
+import "lazysizes";
+import "lazysizes/plugins/respimg/ls.respimg";
 
-import '../../styles/theme.scss';
-import '../../styles/theme.scss.liquid';
+import "../../styles/theme.scss";
+import "../../styles/theme.scss.liquid";
 
-import '../custom/home-testimonials';
-import '../custom/featured-collection';
-import '../custom/tabs';
-import '../custom/product-related';
-import '../custom/different-mentions';
-import '../custom/testimonials';
-import '../custom/faq';
-import '../custom/information-center';
-import '../custom/header';
-import '../custom/image-compare';
-import '../custom/home-stories';
-import '../custom/register';
-import '../custom/account';
+import "../custom/home-testimonials";
+import "../custom/featured-collection";
+import "../custom/tabs";
+import "../custom/product-related";
+import "../custom/different-mentions";
+import "../custom/testimonials";
+import "../custom/faq";
+import "../custom/information-center";
+import "../custom/header";
+import "../custom/image-compare";
+import "../custom/home-stories";
+import "../custom/register";
+import "../custom/account";
+import "../custom/qty-changer";
 
-import { focusHash, bindInPageLinks } from '@shopify/theme-a11y';
-import { cookiesEnabled } from '@shopify/theme-cart';
+import { focusHash, bindInPageLinks } from "@shopify/theme-a11y";
+import { cookiesEnabled } from "@shopify/theme-cart";
 
 // Common a11y fixes
 focusHash();
@@ -32,7 +33,7 @@ bindInPageLinks();
 // Apply a specific class to the html element for browser support of cookies.
 if (cookiesEnabled()) {
   document.documentElement.className = document.documentElement.className.replace(
-    'supports-no-cookies',
-    'supports-cookies'
+    "supports-no-cookies",
+    "supports-cookies",
   );
 }
