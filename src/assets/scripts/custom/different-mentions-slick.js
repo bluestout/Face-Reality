d2fd462c159ac7d9c0319a4cb588c8ac6aa1e01b
Matjaz Zorko <matjaz@apto.si>
2018-11-27 13:29:39 +0100

    Testimonials page & different mentions section responsive

--- src/assets/scripts/custom/different-mentions-slick.js
+++ src/assets/scripts/custom/different-mentions-slick.js
@@ -2,18 +2,40 @@ import $ from "jquery";
 import "slick-carousel";
 
 function mentions() {
-  const $mentions = $(".js-different-mentions-slider");
+  const $mentions = $("[data-different-mentions-slick]");
   if ($mentions.length > 0) {
-    $mentions.slick({
-      swipeToSlide: true,
-      arrows: true,
-      dots: true,
-      slidesToShow: 5,
-      nextArrow:
-        "<button type='button' class='slick-next slick-arrow icon-arrow-right'></button>",
-      prevArrow:
-        "<button type='button' class='slick-prev slick-arrow icon-arrow-left'></button>",
-    });
+    if ($(window).width() > 767) {
+      $mentions.slick({
+        swipeToSlide: true,
+        arrows: true,
+        dots: true,
+        slidesToShow: 5,
+        nextArrow:
+          "<button type='button' class='slick-next slick-arrow icon-arrow-right'></button>",
+        prevArrow:
+          "<button type='button' class='slick-prev slick-arrow icon-arrow-left'></button>",
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 3,
+            },
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+            },
+          },
+        ],
+      });
+    }
   }
 }
 
