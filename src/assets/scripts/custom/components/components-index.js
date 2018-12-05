9fc8cd5a3fb3121c092586a9bce664ab5a3b169c
Matjaz Zorko <matjaz@apto.si>
2018-12-05 14:32:43 +0100

    Improving js file structure, fix colleciton page pagination/load issue

---
+++ src/assets/scripts/custom/components/components-index.js
@@ -0,0 +1,14 @@
+import "./home-testimonials-slick";
+import "./featured-collection-slick";
+import "./tabs";
+import "./product-related-slick";
+import "./different-mentions-slick";
+import "./testimonials";
+import "./header";
+import "./image-compare";
+import "./qty-changer";
+import "./accordion";
+import "./ajax-add-to-cart";
+import "./case-study-slick";
+import "./responsive-sidemenu";
+import "./search";
---
+++ src/assets/scripts/custom/pages/pages-index.js
@@ -0,0 +1,6 @@
+import "./faq";
+import "./information-center";
+import "./register";
+import "./account";
+import "./collection-page";
+import "./case-studies";
